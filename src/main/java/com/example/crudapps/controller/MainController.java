7e8f419c0509ec5e3b2cb0e824188cfaac9f2633
naveenkumarr7979 <naveenkumarr253@gmail.com>
2026-04-13 14:43:42 +0530

    added contact page help and all small changes

--- src/main/java/com/example/crudapps/controller/MainController.java
+++ src/main/java/com/example/crudapps/controller/MainController.java
@@ -9,6 +9,22 @@ public class MainController {
     @GetMapping("/test")
     public String home()
     {
-        return "hello jyoti baby";
+        return "hello jyoti baby buggu i lovpuk e ";
+    }
+    @GetMapping("/contact")
+    public String contact()
+    {
+        return "hi this is contact page";
+    }
+    @GetMapping("/about")
+    public String about()
+    {
+        return "hi this is about page";
+    }
+
+    @GetMapping("/help")
+    public String help()
+    {
+        return "hi this is help page";
     }
 }
